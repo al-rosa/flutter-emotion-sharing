998f67373f65c6de35a594cfbb807e967cd1a9a4
TOMOFUMI-KONDO <ugax2kontomo0314@gmai.com>
2020-09-27 02:15:09 +0900

    タップでいつの気持ちを表示するか切り替えられるようにした。
    今どの点を選択しているのかわからないので、選択している点をハイライトするように後ほどする。

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_emotion_sharing/components/EmotionChart.dart';
 import 'package:flutter_emotion_sharing/constants.dart';
-import 'package:flutter_emotion_sharing/utils.dart';
 
 class DetailScreen extends StatefulWidget {
   static String id = 'detail_screen';
@@ -19,9 +18,6 @@ class _DetailScreenState extends State<DetailScreen> {
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   List<Map<String, dynamic>> _statusList;
-  Icon _displayIcon;
-  String _displayMemo;
-  DateTime _displayDateTime;
 
   @override
   void initState() {
@@ -39,17 +35,9 @@ class _DetailScreenState extends State<DetailScreen> {
 
     List<Map<String, dynamic>> statuses =
         snapshot.docs.map((e) => e.data()).toList();
-    Map<String, dynamic> latestStatus = statuses[0];
-
-    final int index = bigIcons
-        .indexWhere((icon) => icon['emotion'] == latestStatus['emotion']);
-    final icon = bigIcons[index]['icon'];
 
     setState(() {
       _statusList = statuses;
-      _displayIcon = icon;
-      _displayMemo = latestStatus['memo'];
-      _displayDateTime = (latestStatus['timestamp']).toDate();
     });
   }
 
@@ -58,10 +46,9 @@ class _DetailScreenState extends State<DetailScreen> {
 
     int count = 0;
     final List<EmotionLevel> data = _statusList.map((status) {
-      count++;
       final int level =
           bigIcons.indexWhere((icon) => icon['emotion'] == status['emotion']);
-      return EmotionLevel(count, level);
+      return EmotionLevel(count++, level);
     }).toList();
 
     return [
@@ -81,38 +68,9 @@ class _DetailScreenState extends State<DetailScreen> {
       appBar: AppBar(title: Text(widget.userName + 'の気持ち')),
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(bottom: 20),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        height: 300,
-                        padding: EdgeInsets.only(bottom: 20),
-                        child: EmotionChart(
-                          _createChartData(),
-                          animate: true,
-                        ),
-                      ),
-                      Align(
-                          alignment: Alignment.centerRight,
-                          child: Text(
-                            _displayDateTime != null
-                                ? formatDateTime(_displayDateTime)
-                                : '',
-                            style: TextStyle(fontSize: 16),
-                          )),
-                    ],
-                  ),
-                ),
-                _displayIcon ?? Container(),
-                Text(
-                  _displayMemo != null ? 'コメント：' + _displayMemo : '',
-                  style: TextStyle(fontSize: 18),
-                )
-              ])),
+          child: _statusList != null
+              ? EmotionChart(_statusList, _createChartData(), animate: true)
+              : Container()),
     );
   }
 }
